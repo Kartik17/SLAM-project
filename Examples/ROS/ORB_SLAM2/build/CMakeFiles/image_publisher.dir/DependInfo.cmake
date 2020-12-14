ca4880122d49952dee36c85686b83ce1957b4538
lakshmipranathiMaddirala <lmaddira@andrew.cmu.edu>
2020-12-14 18:10:58 -0500

    ros version

---
+++ Examples/ROS/ORB_SLAM2/build/CMakeFiles/image_publisher.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lakshmi/SLAM/ORB_SLAM2/Examples/ROS/ORB_SLAM2/src/image_publisher.cpp" "/home/lakshmi/SLAM/ORB_SLAM2/Examples/ROS/ORB_SLAM2/build/CMakeFiles/image_publisher.dir/src/image_publisher.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"ORB_SLAM2\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/ros/melodic/include"
+  "../"
+  "../../../.."
+  "../../../../include"
+  "/usr/include/opencv"
+  "/home/lakshmi/SLAM/Pangolin/include"
+  "/home/lakshmi/SLAM/Pangolin/build/src/include"
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
